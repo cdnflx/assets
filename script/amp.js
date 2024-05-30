1191d4c02565aa5bd6743351c07eb0f69bbefd46
ro0ted <32649152+R-o0ted@users.noreply.github.com>
2024-05-31 01:08:46 +0700

    Create amp.js

---
+++ script/amp.js
@@ -0,0 +1,6 @@
+const iframe = document.querySelector('amp-iframe');
+const link = document.querySelector('a');
+const urlParams = location.search;
+const game = '';
+console.log(location.search)
+console.log(link.href)
